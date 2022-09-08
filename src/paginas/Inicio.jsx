6d0e608fa8a686cfb50b6d0369d1cf94e5ff9b69
Jhon <jhon.naib327@gmail.com>
2022-09-08 12:37:00 -0500

    project

---
+++ src/paginas/Inicio.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import Cliente from "../components/Cliente";
+
+const Inicio = () => {
+  const [clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    const obtenerClientesAPI = async () => {
+      try {
+        const url = "http://localhost:4000/clientes";
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        setClientes(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    obtenerClientesAPI();
+  }, []);
+
+
+  const handleEliminar = async id => {
+    const confirmar = confirm("Desea eliminar este registro")
+    
+    if(confirmar){
+      try{
+        const url = `http://localhost:4000/clientes/${id}`
+        const respuesta = await fetch(url, {
+          method: 'DELETE'
+        })
+
+        await respuesta.json
+
+        const arrayClientes = clientes.filter( cliente => cliente.id !== id)
+
+        setClientes(arrayClientes)
+
+        //location.reload() //como que recarga el componente para que ya se quite el registrop eliminado del s
+      }catch(error){
+        console.log(error)
+      }
+     
+    }
+  }
+
+  return (
+    <div>
+      <>
+        <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+        <p className="mt-3">
+          Administra tus clientes
+        </p>
+
+        <table className="w-full mt-5 table-auto shadow">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Nombre</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Empresa</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {clientes.map( cliente => (
+              <Cliente
+                key={cliente.id}
+                cliente={cliente}
+                handleEliminar={handleEliminar}
+              />
+            ))}
+          </tbody>
+        </table>
+        
+      </>
+    </div>
+  );
+};
+
+export default Inicio;
